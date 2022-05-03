6f15db83d5dc62c4732de7b5c10976fd1e6a3963
William Avila <willocoav@example.com>
2022-05-03 11:41:22 -0500

    webpack configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
 
 module.exports = {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'inline-source-map', // codigo legible
   output: {
-    filename: 'bundle.[contenthash].js',
-    clean: true
+    filename: 'bundle.[contenthash].js',// [contenthash] esta sentencia me permite crear un numero con el que se general el paquete, esto limpia cache y permite que los despliegues no se queden pegados.
+    clean: true // limpia la carpeta dist
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'src/index.html'
+  plugins: [new HtmlWebpackPlugin({  //cada vez que compila crea un hash
+    template: 'src/index.html'//el hash lo toma de este modelo
   })],
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx)$/,  
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-react']
+            presets: ['@babel/preset-react'] //loader para react
           }
         }
       },
@@ -34,10 +33,8 @@ module.exports = {
           "sass-loader",
         ],
       },
-    ],
-    
+    ], 
   },
-
   resolve: {
     extensions: ['*', '.js', '.jsx'],// this was necesary to read jsx
   },
